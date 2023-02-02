913fc5204ffef82c6789925b69de11266b5f4133
Héberty Filho <hfmistake@gmail.com>
2023-02-02 17:29:41 -0300

    Added taskbuttons

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,44 +1,49 @@
-window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("DOMContentLoaded", main)
 
 
 function main() {
-  const sendbutton = document.getElementById("sendbutton");
-  let lists = [];
-  let listname;
-  let loaded = JSON.parse(localStorage.getItem("listas"));
+    const sendbutton = document.getElementById("sendbutton")
+    let lists = [];
+    let listname;
+    let loaded = JSON.parse(localStorage.getItem("listas"))
 
-  if (loaded) {
-    for (const el of loaded) {
-      addlist(el)
+
+
+    if (loaded) {
+        for (const el of loaded) {
+            addlist(el)
+        }
     }
-  }
 
-  function addlist(name) {
-    const element = document.createElement("h1")
-    element.innerHTML = name
-    element.id = name
+    function addlist(name) {
+        const taskbutton = document.createElement("button")
+        taskbutton.innerHTML = "Adicionar Tarefa"
+        const element = document.createElement("h1")
+        element.innerHTML = name
+        element.id = name
+        taskbutton.id = name + " button"
+        element.append(taskbutton)
 
-    document.querySelector("body").append(element)
-    lists.push(name)
-    localStorage.setItem("listas", JSON.stringify(lists))
+        document.querySelector("body").append(element)
+        lists.push(name)
+        localStorage.setItem("listas", JSON.stringify(lists))
+        sendbutton.disabled = true
+    }
+
     sendbutton.disabled = true
-  }
 
-  sendbutton.disabled = true
-
-  document.querySelector("#inputlistname").onkeyup = () => {
-    sendbutton.disabled = document.querySelector("#inputlistname").value === "";
-  }
-
-  sendbutton.onclick = () => {
-    listname = document.getElementById("inputlistname").value
-    if (listname !== "") {
-      addlist(listname)
-      document.querySelector("#inputlistname").value = ""
-    } else {
-      alert("Cannot create null list name.")
-      sendbutton.disabled = true
+    document.querySelector("#inputlistname").onkeyup = () => {
+        sendbutton.disabled = document.querySelector("#inputlistname").value === ""
     }
 
-  }
+    sendbutton.onclick = () => {
+        listname = document.getElementById("inputlistname").value
+        if (listname !== "") {
+            addlist(listname)
+            document.querySelector("#inputlistname").value = ""
+        } else {
+            alert("Invalid list name {null}.")
+            sendbutton.disabled = true
+        }
+    }
 }
